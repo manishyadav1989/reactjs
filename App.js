fd2199bbe5896b0f4f8f560ffb22ed6902ba7f30
Manish Kumar <yuvrajdhl@gmail.com>
2024-05-24 23:06:14 +0530

    Add JSX in react

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from "react"
 import ReactDom from "react-dom/client";
 
-const heading = React.createElement(
-    "div",
-    {id:"parent", className:"parent"}, 
-    React.createElement(
-        "div",
-        {id:"child", className:"child"},
-        React.createElement(
-            "h1",
-            {id:"heading", className:"heading"}, 
-            "Welcome into the React Js world!"
-        )
-    )
-);
-console.log(heading);
+// const heading = React.createElement(
+//     "div",
+//     {id:"parent", className:"parent"}, 
+//     React.createElement(
+//         "div",
+//         {id:"child", className:"child"},
+//         React.createElement(
+//             "h1",
+//             {id:"heading", className:"heading"}, 
+//             "Welcome into the React Js world!"
+//         )
+//     )
+// );
+//console.log(heading);
+const jsxHeading = <h1 id="heading">Welcome into the React Jsx world!</h1>;
+console.log(jsxHeading);
 const MainApp = ReactDom.createRoot(document.getElementById('app'));
-MainApp.render(heading);
+MainApp.render(jsxHeading);
